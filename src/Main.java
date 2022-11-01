ffb7f18fb05197b9476873a442a4acff9ff920e0
Дамир <damir.obuhov@mail.ru>
2022-11-02 01:19:56 +0400

    готово

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a=67;
+        double b= 27.12;
+        float c= 546;
+        int d = - 156;
+        long e =27897;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
